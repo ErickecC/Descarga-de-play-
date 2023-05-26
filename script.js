89b79cb8b30cb225754ea617248355ed7af75435
Studio-Web <127244087+ErickecC@users.noreply.github.com>
2023-05-25 23:02:18 -0500

    Versión uno
    Falta para descargar

---
+++ script.js
@@ -0,0 +1,8 @@
+function formatosapp() {
+ const contenido = document.getElementById("oculto-visible") 
+  if (contenido.style.display === "none") { contenido.style.display = "block"; 
+ }
+else {
+ contenido.style.display = "none"; 
+  } 
+}
